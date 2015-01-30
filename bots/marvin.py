7960dc2460df8c1677539a538fcedfc721bf9373
Alex J. Champandard <alexjc@aigamedev.com>
2015-01-30 14:06:27 +0100

    Hack for speech-to-text and text-to-speech in The Resistance.  Marvin answers basic questions about the game state, no creativity or humour yet... Some issues with lag in the Google Speech API.

--- bots/marvin.py
+++ bots/marvin.py
@@ -1,5 +1,7 @@
 import os
+import random
 import threading
+import subprocess
 
 import speech_recognition as speech
 
@@ -12,7 +14,7 @@ class Marvin(experts.Suspicious):
         super(Marvin, self).__init__(*args, **kwargs)
 
         self.recognizer = speech.Recognizer()
-        self.recognizer.energy_threshold = 4000
+        self.recognizer.energy_threshold = 1000
         self.stop = False
 
         self.thread = threading.Thread(target=self.listen)
@@ -23,42 +25,46 @@ class Marvin(experts.Suspicious):
         self.stop = True
         self.thread.join()
 
-    def say(self, message):
-        super(Marvin, self).say(message)
-        os.system('/usr/bin/say -v Zarvox "%s"' % message)
+    def _say(self, message):
+        # os.system('/usr/bin/say -v Zarvox "%s"' % message)
+        print "saying..."
+        subprocess.call(['/usr/bin/say', '-v', 'Zarvox', "%s" % message])
+        print ".said!"
 
     def listen(self):
         while not self.stop:
             try:
                 with speech.Microphone() as source:
                     print "Listening...",
-                    audio = self.recognizer.listen(source, timeout=5.0)
+                    audio = self.recognizer.listen(source)
                     print "DONE! (%i)" % len(audio.data)
                 try:
                     st = self.recognizer.recognize(audio).lower()
-                    print ">", st
+                    print "<<", st
                     for prefix in ["i am", "this is"]:
                         if st.startswith(prefix):
                             name = st.replace(prefix, '')
-                            self.say("Hello %s!" % name)
-                            return
+                            self._say("Hello %s!" % name)
+                            continue
                     if "about" in st:
+                        print "QUERY:"
                         self.respondToQuery(st)
-                        return
+                        continue
                     if 'cool' in st:
-                        self.say("Robots are always cool!")
-                        return
+                        self._say("Robots are always cool!")
+                        continue
+                    if random.random() > 0.75:
+                        self._say("What a rust bucket!")
+                    print ">>"
                 except LookupError:
                     print "Sorry."
-            except NameError:
-                pass
             except:
                 import traceback
                 traceback.print_exc()
 
     def _extractPlayers(self, message):
         def matches(p):
-            return p.name in message or ("#%i" % p.index) in message
+            return p.name.lower() in message or ("#%i" % p.index) in message
         return set([p for p in self.game.players if matches(p)])
 
     def onMessage(self, source, message):
@@ -72,8 +78,10 @@ class Marvin(experts.Suspicious):
         players = self._extractPlayers(message) - set([self])
         configs = self.likeliest()
         if len(players) != 1:
+            print "no players"
             return
 
+        print "configs", configs
         # Determine how player fits into most likely configurations.
         p = players.pop()
         spy_score, res_score = 0, 0
@@ -83,10 +91,18 @@ class Marvin(experts.Suspicious):
 
         # Express beliefs about the current estimates for the player.
         if spy_score == res_score:
-            self.say("It's unclear at this stage what %r is playing." % (p.name))
+            self._say("%r has high entropy." % (p.name))
         if spy_score > res_score:
-            confidence = "arguably" if res_score > 0 else "likely"
-            self.say("I think %r is %s a spy at this stage." % (p.name, confidence))
+            if res_score > 0:
+                # arguably
+                self._say("%r has some suspicious bits." % (p.name))
+            else:
+                # likely
+                self._say("%r should optimize its spy policy." % (p.name))
         if res_score > spy_score:
-            confidence = "arguably" if spy_score > 0 else "likely"
-            self.say("I think %r is %s resistance at this stage." % (p.name, confidence))
+            if spy_score > 0:
+                # arguably
+                self._say("%r's bits are somewhat clean." % (p.name))
+            else:
+                # likely
+                self._say("%r follows a resistance policy." % (p.name))                
