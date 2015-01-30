efdc798a923cee3200db66b17d33b6a41766bfbe
Alex J. Champandard <alexjc@aigamedev.com>
2015-01-30 09:43:12 +0100

    Integrating query/responses into the speech recognition and vocalization.

--- bots/marvin.py
+++ bots/marvin.py
@@ -1,14 +1,15 @@
 import os
-import experts
 import threading
 
 import speech_recognition as speech
 
+import experts
 
-class Zarvox(experts.Suspicious):
+
+class Marvin(experts.Suspicious):
 
     def __init__(self, *args, **kwargs):
-        super(Zarvox, self).__init__(*args, **kwargs)
+        super(Marvin, self).__init__(*args, **kwargs)
 
         self.recognizer = speech.Recognizer()
         self.recognizer.energy_threshold = 4000
@@ -22,6 +23,10 @@ class Zarvox(experts.Suspicious):
         self.stop = True
         self.thread.join()
 
+    def say(self, message):
+        super(Marvin, self).say(message)
+        os.system('/usr/bin/say -v Zarvox "%s"' % message)
+
     def listen(self):
         while not self.stop:
             try:
@@ -30,17 +35,23 @@ class Zarvox(experts.Suspicious):
                     audio = self.recognizer.listen(source, timeout=5.0)
                     print "DONE! (%i)" % len(audio.data)
                 try:
-                    st = self.recognizer.recognize(audio)
+                    st = self.recognizer.recognize(audio).lower()
                     print ">", st
-                    if st.lower().startswith("i am"):
-                        name = st[5:]
-                        os.system('/usr/bin/say -v Zarvox "Hello %s!"' % name)
+                    for prefix in ["i am", "this is"]:
+                        if st.startswith(prefix):
+                            name = st.replace(prefix, '')
+                            self.say("Hello %s!" % name)
+                            return
+                    if "about" in st:
+                        self.respondToQuery(st)
                         return
-                    if 'cool' in st.lower():
-                        os.system('/usr/bin/say -v Zarvox "Robots are always cool\!"')
+                    if 'cool' in st:
+                        self.say("Robots are always cool!")
                         return
                 except LookupError:
                     print "Sorry."
+            except NameError:
+                pass
             except:
                 import traceback
                 traceback.print_exc()
@@ -55,6 +66,9 @@ class Zarvox(experts.Suspicious):
         if not message.startswith(self.name) or 'about' not in message:
             return
 
+        self.respondToQuery(message)
+
+    def respondToQuery(self, message):
         players = self._extractPlayers(message) - set([self])
         configs = self.likeliest()
         if len(players) != 1:
@@ -69,10 +83,10 @@ class Zarvox(experts.Suspicious):
 
         # Express beliefs about the current estimates for the player.
         if spy_score == res_score:
-            self.say("It's unclear at this stage what %r is playing." % (p))
+            self.say("It's unclear at this stage what %r is playing." % (p.name))
         if spy_score > res_score:
             confidence = "arguably" if res_score > 0 else "likely"
-            self.say("I think %r is %s a spy at this stage." % (p, confidence))
+            self.say("I think %r is %s a spy at this stage." % (p.name, confidence))
         if res_score > spy_score:
             confidence = "arguably" if spy_score > 0 else "likely"
-            self.say("I think %r is %s resistance at this stage." % (p, confidence))
+            self.say("I think %r is %s resistance at this stage." % (p.name, confidence))
